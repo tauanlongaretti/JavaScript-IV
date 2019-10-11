272ee809cd6b154792055fb86aa534ff8971572c
tauan-longaretti <tauanlongaretti@gmail.com>
2019-10-10 23:27:57 -0400

    concluded most of the code for lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,51 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.age;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAttributes) {
+        super(instructorAttributes);
+        this.speciality = instructorAttributes.speciality;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`)
+    }
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttributes) {
+        super(studentAttributes);
+        this.previousBackground = studentAttributes.previousBackground;
+        this.className = studentAttributes.className;
+    }
+    listsSubjects() {
+    }
+    PRAssignment(subject) {
+        console.log(`${student.name} has submitted a PR for ${subject}`)
+    }
+    sprintChallenge(subject) {
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(ManagerAttributes) {
+        super(ManagerAttributes);
+        this.gradClassName = ManagerAttributes.gradClassName;
+        this.favInstructor = ManagerAttributes.favLanguage;
+    }
+}
