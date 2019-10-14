c79798323923a4636ec50aee40fa38baa5d068f7
tauan-longaretti <tauanlongaretti@gmail.com>
2019-10-13 20:56:41 -0400

    Finished lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -31,8 +31,12 @@ class Student extends Person {
         super(studentAttributes);
         this.previousBackground = studentAttributes.previousBackground;
         this.className = studentAttributes.className;
+        this.favSubjects = studentAttributes.favSubjects;
     }
     listsSubjects() {
+        this.favSubjects.forEach(subject => {
+            console.log(`${subject}`);
+        });
     }
     PRAssignment(subject) {
         console.log(`${student.name} has submitted a PR for ${subject}`)
@@ -46,6 +50,85 @@ class ProjectManager extends Instructor {
     constructor(ManagerAttributes) {
         super(ManagerAttributes);
         this.gradClassName = ManagerAttributes.gradClassName;
-        this.favInstructor = ManagerAttributes.favLanguage;
+        this.favInstructor = ManagerAttributes.favInstructor;
+    }
+    standUp(){
+        console.log(`${this.name} announces to ${gradClassName}, @channel standy times!`);
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
+
+//Lambda school personnel
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    speciality: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  const jason = new Instructor({
+    name: 'Jason',
+    location: 'Trenton',
+    age: 28,
+    favLanguage: 'Python',
+    speciality: 'Back-end',
+    catchPhrase: `Hard work is key`
+  });
+
+  const ana = new Instructor({
+    name: 'Ana',
+    location: 'Orlando',
+    age: 32,
+    favLanguage: 'React',
+    speciality: 'Front-end',
+    catchPhrase: `Less is more`
+  });
+
+  const tommy = new Student({
+    name: 'Tommy',
+    age: 21,
+    location: 'Seattle',
+    previousBackground: 'Graphic Design',
+    className: 'WEBPT10',
+    favSubjects: ["HTML", "Javascript"],
+  });  
+
+  const hellen = new Student({
+    name: 'Hellen',
+    age: 24,
+    location: 'Boston',
+    previousBackground: 'Sculptor',
+    className: 'WEBPT8',
+    favSubjects: ["CSS", "Java"],
+  });  
+
+  const jack = new ProjectManager({
+    name: 'Jack',
+    age: 30,
+    location: 'Toronto',
+    gradClassName: 'WEBPT9_jack',
+    favInstructor: 'Ana',
+  });
+
+  const daniella = new ProjectManager({
+    name: 'Daniella',
+    age: 36,
+    location: 'Houston',
+    gradClassName: 'WEBPT10_daniella',
+    favInstructor: 'Jason',
+  });
+
+  console.log(fred.speciality);
+  console.log(jason.favLanguage);
+  console.log(ana.catchPhrase);
+  console.log(tommy.previousBackground);
+  console.log(hellen.className);
+  console.log(jack.gradClassName);
+  console.log(daniella.favInstructor);
+
+
